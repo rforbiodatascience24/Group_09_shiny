9a88fab7cabcd8bc00afae00a2b1e69eef370d7e
AndreisMarco <andreismarco2002gmail.com>
2024-11-05 10:42:45 +0000

    Updated the main file and the app_funcions fiel

---
+++ virtual_central_dogma.R
@@ -0,0 +1,20 @@
+# full app calling the single components
+
+# Load needed libraries
+library("bslib")
+library("shiny")
+library("stringr")
+library("ggplot2")
+
+# Load needed functions
+source(file = "app_functions.R")
+
+# Run the frontend user interface ui.R
+source(file = "ui.R")
+
+# Run the backend server server.R
+source(file = "server.R")
+
+# Run the Shiny app
+shinyApp(ui = ui, server = server)
+
